599ea5f38e57c47ace3dff1d08ed05cdf7b8ea5d
jairaaman006 <99857978+jairaaman006@users.noreply.github.com>
2022-02-25 09:30:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//1. Describe the below code.
+console.log(typeof(1));
+// number 
+console.log(typeof(1.1));
+// number
+console.log(typeof('1.1'));
+//string
+console.log(typeof(true));
+//boolean
+console.log(typeof(null));
+//object
+console.log(typeof(undefined));
+//undefined
+console.log(typeof([]));
+//object
+console.log(typeof({}));
+//object
+console.log(typeof(NaN));
+// number
